ff58b0d7d78b0a2fee41460339b87bda6ba169ed
lbreck93 <lbreck93@gmail.com>
2015-09-26 16:54:48 -0500

    Added password complexity, made login form, added to index page.

--- MyRelevate/forms.py
+++ MyRelevate/forms.py
@@ -1,13 +1,25 @@
 from django import forms
 from .models import User
+from passwords.fields import PasswordField
+from passwords.validators import LengthValidator, ComplexityValidator
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput, label="Email")
+    password = PasswordField(widget=forms.PasswordInput, label='Password')
+
+    class Meta:
+        fields = ['email', 'password']
 
 
 class RegistrationForm(forms.ModelForm):
     email = forms.EmailField(widget=forms.TextInput, label='Email')
-    password1 = forms.CharField(widget=forms.PasswordInput, label='Password')
+    password1 = PasswordField(widget=forms.PasswordInput, label='Password', validators=[{
+        LengthValidator(min_length=5), ComplexityValidator(complexities=dict(UPPER=1, LOWER=1, DIGITS=1))
+    }])
     password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
-    first_name = forms.CharField(widget=forms.CharField, max_length=50, label='First name')
-    last_name = forms.CharField(widget=forms.CharField, max_length=100, label='Last name')
+    first_name = forms.CharField(widget=forms.TextInput, max_length=50, label='First name')
+    last_name = forms.CharField(widget=forms.TextInput, max_length=100, label='Last name')
 
     class Meta:
         model = User
