634faa00deadd9e205917a6953054bd4a06f8081
Jeremy <jeremyrt88@gmail.com>
2015-09-26 16:19:15 -0500

    Created url routes. Built a base index page.

--- SeniorProject/urls.py
+++ SeniorProject/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^MyRelevate/', include('MyRelevate.urls', namespace='MyRelevate')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
