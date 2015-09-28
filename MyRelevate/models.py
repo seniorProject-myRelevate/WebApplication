fb9d929d274083f4e6607789d5193da61331f597
lbreck93 <lbreck93@gmail.com>
2015-09-27 22:27:18 -0500

    building usermanager, not done yet.
    updated requirements to include django.passwords.

--- MyRelevate/models.py
+++ MyRelevate/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name):
+        user = self.model(username=email, first_name=first_name,
+                          last_name=last_name)
+        return user
 
 
 class User(AbstractBaseUser):
