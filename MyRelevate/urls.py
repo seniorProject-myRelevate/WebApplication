dd4f55a9d456d64ce82d194c1f422f5041302604
lbreck93 <lbreck93@gmail.com>
2015-10-14 23:05:15 -0500

    redirected login link to the login page, also passed user to the view.

--- MyRelevate/urls.py
+++ MyRelevate/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^register/$', views.register_user, name="register_user"),
     url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
 ]
 
