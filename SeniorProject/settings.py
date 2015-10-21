8a883fc9bef2a7e5139132eabdc31be97bcd8e94
richtarj <richtarj@gmail.com>
2015-10-21 10:07:59 -0500

    db migration rollback

--- SeniorProject/settings.py
+++ SeniorProject/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'SeniorProject.wsgi.application'
 
 DATABASES = {
     'default':{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'relevate',
-        'USER': 'relevateadmin',
-        'PASSWORD': 'r3l3vat3',
-        'HOST': 'relevate.cdxbllcvsaza.us-west-2.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
