90417077d307fa398b2cdc9760cf73b072d36d88
Jeremy <jeremyrt88@gmail.com>
2015-11-11 22:34:43 -0600

    added javascript for subscribe button

--- MyRelevate/static/js/main.js
+++ MyRelevate/static/js/main.js
@@ -5,6 +5,11 @@ var requestAccessDiv = document.getElementById('requestAccess');
 var registrationBtn = document.getElementById('registration');
 var requestBtn = document.getElementById('request');
 var subscribeDiv = document.getElementById('subscribe');
+var welcomeDiv = document.getElementById('welcome');
+var emailDiv = document.getElementById('id_email');
+var email = document.getElementById('id_subscriptioninput');
+
+email.addEventListener('click', Subscribe, true);
 
 /*
 
@@ -12,15 +17,56 @@ var subscribeDiv = document.getElementById('subscribe');
 function ContributorAccess() {
     contributorsDiv.style.display = 'none';
     requestAccessDiv.style.display = 'block';
-
     requestBtn.onclick = function () {
 
     }
 }
 
 /*
+ $("id_subscriptioninput").click(function(){
+ window.location = "https://www.google.com";
+ return false;
+ });
+ */
+
+/*
 
  */
-function Subscribe() {
-    subscribeDiv.style.display = 'block';
+function Subscribe(event) {
+
+    event.preventDefault();
+
+    if (email.form.valueOf("")) {
+        subscribeDiv.style.display = 'none';
+        welcomeDiv.style.display = 'block';
+    }
+    else {
+
+    }
+
+    //window.location.replace("https://www.google.com");
+    if (emailDiv.value === "") {
+        //subscribeDiv.style.display = 'none';
+        //welcomeDiv.style.display = 'block';
+    }
+    else {
+        //window.location = "https://www.google.com"
+        //subscribeDiv.style.display = 'none';
+        //welcomeDiv.style.display = 'block';
+    }
+
+
+    //window.location.replace("https://www.google.com");
+    //window.location = "https://www.google.com";
+    //console.log('here');
+    /*
+     if ($("input").val()!= ""){
+     subscribeDiv.style.display = 'none';
+     welcomeDiv.style.display = 'block';
+     window.location = "/subscribe"
+     }
+     else{
+
+     }
+     */
 }
