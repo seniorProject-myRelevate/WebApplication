08b31155f784b11dbbc8b381dd27bf4c47404941
Jeremy <jeremyrt88@gmail.com>
2015-11-13 11:30:26 -0600

    worked on saving data to database
    worked in main.js

--- MyRelevate/static/js/main.js
+++ MyRelevate/static/js/main.js
@@ -4,11 +4,38 @@ var contributorsDiv = document.getElementById('profiles');
 var requestAccessDiv = document.getElementById('requestAccess');
 var registrationBtn = document.getElementById('registration');
 var requestBtn = document.getElementById('request');
-var subscribeDiv = document.getElementById('subscribe');
-var welcomeDiv = document.getElementById('welcome');
 var email = document.getElementById('id_subscriptioninput');
 
 email.addEventListener('click', Subscribe, true);
+
+
+// AJAX for posting
+function create_post() {
+    console.log("create post is working!") // sanity check
+    console.log($('#id_email').val())
+    $.ajax({
+        //url: "", // the endpoint
+        type: "POST", // http method
+        data: {the_post: $('#id_email').val()}, // data sent with the post request
+
+        // handle a successful response
+        success: function (json) {
+            $('#id_email').val('Thank you for signing up!'); // remove the value from the input
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+        }//,
+    });
+    /*
+     // handle a non-successful response
+     error : function(xhr,errmsg,err) {
+     $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+     " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+     console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+     }
+     });
+     */
+};
+
 
 /*
 
@@ -25,14 +52,59 @@ function ContributorAccess() {
 
  */
 function Subscribe(event) {
-
     event.preventDefault();
-
-    if (email.form.value === "") {
-        subscribeDiv.style.display = 'none';
-        welcomeDiv.style.display = 'block';
-    }
-    else {
-        window.location.reload();
-    }
+    console.log("form submitted");
+    create_post();
 }
+
+
+// This function gets cookie with a given name
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+/*
+ The functions below will create a header with csrftoken
+ */
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+function sameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+        !(/^(\/\/|http:|https:).*/.test(url));
+}
+
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+            // Send the token to same-origin, relative URLs only.
+            // Send the token only if the method warrants CSRF protection
+            // Using the CSRFToken value acquired earlier
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
