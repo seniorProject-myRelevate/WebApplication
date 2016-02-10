6cd5a85140c31b6c34fe8130458061586b7469e7
Gunnarr07 <jeremyrt88@gmail.com>
2016-02-09 21:03:36 -0600

    Reworked contributor access form worked on styling for contributor access form added drop down list worked on contributor profile layout

--- MyRelevate/urls.py
+++ MyRelevate/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.subscribe, name="subscribe"),
+    #url(r'^$', views.subscribe, name="subscribe"),
+    url(r'^$', views.index, name="index"),
     url(r'^(?i)index$', views.index, name="index"),
     url(r'^(?i)register/$', views.register_user, name="register_user"),
     url(r'^(?i)confirm/(?P<token>[\w\.]+)/$', views.confirm, name="confirm"),
