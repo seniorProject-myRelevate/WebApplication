849c8cec5f687306a0045aa0f20cb13893d40a68
lbreck93 <Lbreck93@gmail.com>
2016-03-24 12:11:13 -0500

    Update urls.py

--- MyRelevate/urls.py
+++ MyRelevate/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.subscribe, name="subscribe"),
-    url(r'^(?i)index$', views.index, name="index"),
+    url(r'^(?i)index/$', views.index, name="index"),
     url(r'^(?i)register/$', views.register_user, name="register_user"),
     url(r'^(?i)new_confirm/$', views.new_confirm, name="new_confirm"),
     url(r'^(?i)confirm/$', views.confirm, name="confirm"),
