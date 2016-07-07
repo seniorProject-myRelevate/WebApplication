421a604586675451fa7d53fe8ef167954312a506
Jeremy <jeremyrt88@gmail.com>
2016-07-06 23:24:15 -0500

    work on displaying expertise topics

--- MyRelevate/models.py
+++ MyRelevate/models.py
@@ -25,8 +25,17 @@ class Subscriber(models.Model):
 
 
 class Topics(models.Model):
+    class Meta:
+        db_table = 'topic'
     topicName = models.CharField(max_length=100, null=False, blank=False)
     topicDescription = models.CharField(max_length=255, null=False, blank=False)
+
+
+class ContributorTopics(models.Model):
+    class Meta:
+        db_table = 'contributortopics'
+    contributor_profile = models.ForeignKey(ContributorProfile, blank=True, unique=True)
+    topic_choices = models.ForeignKey(Topics)
 
 
 # class Adviser(models.Model):
