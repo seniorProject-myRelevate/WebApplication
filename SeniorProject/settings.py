ebead46a552feda33df001c2150c67904b83219b
Jeremy <jeremyrt88@gmail.com>
2016-08-11 13:56:00 -0500

    worked on contributor save

--- SeniorProject/settings.py
+++ SeniorProject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SecretKey']
-SECRET_KEY = "SecretKey"
+SECRET_KEY = os.environ['SecretKey']
+# SECRET_KEY = "SecretKey"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ['Debug']
-DEBUG = True
+DEBUG = os.environ['Debug']
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
